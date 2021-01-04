4aa45b9bb45106c231857185c70bebc5b0446c5d
jongsung-jay <jongsung.jay@gmail.com>
2021-01-03 23:15:12 -0500

    c#

---
+++ C#/06_method/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        // method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
