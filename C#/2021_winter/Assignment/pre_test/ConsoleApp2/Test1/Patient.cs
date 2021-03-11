d1f434a959afa5d5ddbc9527a352ddb6e9fa8583
jongsung-jay <jongsung.jay@gmail.com>
2021-03-10 20:53:51 -0500

    assign

---
+++ C#/2021_winter/Assignment/pre_test/ConsoleApp2/Test1/Patient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Test1
+{
+    class Patient
+    {
+        // Field
+        private List<Prescription> prescriptions = new List<Prescription>();
+
+        // Properties
+        public string Name { get; private set; }
+        public int Yob { get; private set; }
+
+        // Constructor
+        public Patient(string name, int yob) {
+            Name = name;
+            Yob = yob;
+        }
+
+        // Methods
+        public void AddPrescription(Prescription prescription) {
+            prescriptions.Add(prescription);
+        }
+
+        public void RemovePrescription(string id) {
+            bool found = false;
+            try {
+                for (int index = 0; index < prescriptions.Count; index++)
+                {
+                    if (prescriptions[index].ID.ToString() == id)
+                    {
+                        found = true;
+                        prescriptions.RemoveAt(index);
+                    }
+                }
+            }
+            catch(Exception e) {
+                if (!found)
+                {
+                    Console.WriteLine($"Exception:" + e.Message);
+                }
+            }  
+        }
+
+        private string GetPrescriptions() {
+            string result = "";
+            for (int index = 0; index < prescriptions.Count; index++) {
+                result += prescriptions[index].ToString() + "\n";
+            }
+            return result;
+        }
+
+        public override string ToString() {
+            string result = $"{Name} yob {Yob}";
+            result += $"List of prescription \n {GetPrescriptions()}";
+            return result;
+        }
+
+        public void SaveAsText(string filename) {
+            using (StreamWriter outputFile = new StreamWriter(filename)) {
+                outputFile.WriteLine(ToString());
+            }
+        }
+    }
+}
---
+++ C#/2021_winter/Assignment/pre_test/ConsoleApp2/Test1/Prescription.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test1
+{
+    class Prescription
+    {
+        //Field
+        private static int CURRENT_NUMBER = 100;
+        public string ID { get; private set; }
+
+        // properties
+        public bool ForRepeat { get; private set; }
+        public string Doctor { get; private set; }
+        public string Name { get; private set; }
+        public int Length { get; private set; }
+
+        // constructor
+        public Prescription(string doctor, string name, int length, bool forRepeat = false) {
+            Doctor = doctor;
+            Name = name;
+            Length = length;
+            ForRepeat = forRepeat;
+            ID = CURRENT_NUMBER.ToString();
+            CURRENT_NUMBER++;
+        }
+
+        // Methods
+        public override string ToString() {
+            string result = $"#{ID} {Name} prescribed by {Doctor} for {Length} days \n";
+            if (ForRepeat)
+            {
+                result += "{Repeat}";
+            }
+            else {
+                result += "(No repeat)";
+            }
+            return result;
+        }
+    }
+}
