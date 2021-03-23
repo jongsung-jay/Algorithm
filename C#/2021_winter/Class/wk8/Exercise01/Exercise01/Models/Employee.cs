4ec89bf7e183c7ca27589cdc50d6264c982ac1fb
jongsung-jay <jongsung.jay@gmail.com>
2021-03-23 00:34:16 -0400

    wk8_abstract&virtual

--- C#/2021_winter/Class/wk8/Exercise01/Exercise01/Models/Employee.cs
+++ C#/2021_winter/Class/wk8/Exercise01/Exercise01/Models/Employee.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace Exercise01.Models
 {
-    class Employee
+    public abstract class Employee
     {
+        public string EmployeeID { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+        public double Salary { get; set; }
+        public string Position { get; set; }
+
+        public void ClockIn() {
+            Console.WriteLine($"{this.EmployeeID} clocked in");
+        }
+
+        public void Clockout() {
+            Console.WriteLine($"{this.EmployeeID} clocked out");
+        }
+
+        public abstract void Work(); // abstract method cannot have body
+
+        public virtual void ReceivePay() {
+            Console.WriteLine("Just got paid!");
+        }
+
+        private void Quit() { 
+            // 다른 클래스에서 접근하기위해서는 private이 아닌 protected를 사용하면 됨. 상속경우.
+        }
     }
 }
--- C#/2021_winter/Class/wk8/Exercise01/Exercise01/Program.cs
+++ C#/2021_winter/Class/wk8/Exercise01/Exercise01/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Exercise01.Models;
 
 namespace Exercise01
 {
@@ -6,7 +8,33 @@ namespace Exercise01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Employee employee = new Employee(); //employee 슈퍼 클래스를 abstract로 해주면 삭제한후 밑의 클래스들을 저렇게 쓰는게 가능해짐
+            Employee salesPerson = new SalesPerson() { // salesPerson은 항상 employee가되지만 employee는 항상 salesPerson이 되지 않음 so, Employee salesPerson = new SalesPerson(); 이게 가능한것
+                EmployeeID = "SALES01",
+                FristName = "John",
+                LastName = "Smith"
+            }; 
+
+            Employee genitor = new Genitor() {
+                EmployeeID = "GEN01",
+                FristName = "Steve",
+                LastName = "Johnson",
+                Shift = Shift.Night
+            };
+
+            //List<Employee> list = new List<Employee>();
+            //list.Add(salesPerson); //employee와 salesperson이 같게됨
+
+            salesPerson.ClockIn();
+            salesPerson.Clockout();
+
+            Console.WriteLine("\n========================================================\n");
+            salesPerson.Work();
+            genitor.Work();
+            Console.WriteLine("\n========================================================\n");
+            salesPerson.ReceivePay();
+            genitor.ReceivePay();
         }
     }
+
 }
